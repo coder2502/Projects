bef29aa0e76defe8fd732abfb451eac9fccd0de1
coder2502 <81462548+coder2502@users.noreply.github.com>
2022-12-01 11:06:47 +0530

    Update UserServices.java

--- mini-project/src/main/java/com/myMiniProject/javaspringboot/miniproject/user/UserServices.java
+++ mini-project/src/main/java/com/myMiniProject/javaspringboot/miniproject/user/UserServices.java
@@ -33,20 +33,14 @@ public class UserServices {
 		return users.stream().filter(predicate).findFirst().orElse(null);
 	}
 	
-//	public User findOne(String name) {
-//		Predicate<? super User> predicate = user -> user.getName().equals(name);
-//		return users.stream().filter(predicate).findFirst().orElse(null);
-//	}
+
 
 	public void deleteById(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		users.removeIf(predicate);
 	}
 
-//	public void deleteByName(String name) {
-//		Predicate<? super User> predicate = user -> user.getName().equals(name);
-//		users.removeIf(predicate);
-//	}
+
 	
 	public User filterUserByBloodGroup(String bloodGroup) {
 		Predicate<? super User> predicate = user -> user.getBloodGroup().equals(bloodGroup);
